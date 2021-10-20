6f61d8f8a02098d35f8ae920ed4f6229895322d1
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-20 23:42:11 +0530

    Made Layouts

--- app/src/main/java/com/example/githubokhttpissueslist/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/githubokhttpissueslist/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.githubokhttpissueslist
+package com.example.githubokhttpissueslist.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githubokhttpissueslist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
