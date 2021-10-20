fe9d30038abe9f83868edf863f61d9f5ad88118f
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-21 02:06:42 +0530

    Connected Adapter & MainActivity

--- app/src/main/java/com/example/githubokhttpissueslist/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/githubokhttpissueslist/ui/view/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubokhttpissueslist.R
+import com.example.githubokhttpissueslist.data.models.Issues
 import com.example.githubokhttpissueslist.data.repos.GithubRepository
+import com.example.githubokhttpissueslist.ui.adapter.IssueAdapter
 import com.example.githubokhttpissueslist.ui.viewModel.GithubViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,13 +18,24 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this).get(GithubViewModel::class.java)
     }
 
+    private val list = arrayListOf<Issues>()
+
+    val adapter = IssueAdapter(list)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        issuesRv.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = this@MainActivity.adapter
+        }
         vm.fetchIssues()
         vm.issues.observe(this, Observer {
-
+            if (!it.isNullOrEmpty()) {
+                list.addAll(it)
+                adapter.notifyDataSetChanged()
+            }
         })
     }
 }
