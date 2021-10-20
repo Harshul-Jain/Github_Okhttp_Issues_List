e1b92c9044e416f9ee200260057d5f8f76755939
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-21 00:18:23 +0530

    Started MainActivity.kt

--- app/src/main/java/com/example/githubokhttpissueslist/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/githubokhttpissueslist/ui/view/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.githubokhttpissueslist.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.githubokhttpissueslist.R
+import com.example.githubokhttpissueslist.data.repos.GithubRepository
+import com.example.githubokhttpissueslist.ui.viewModel.GithubViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    val vm by lazy {
+        ViewModelProvider(this).get(GithubViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vm.fetchIssues()
+        vm.issues.observe(this, Observer {
+
+        })
     }
 }
